4c093289f644bf7522238bba78224beab833ea9a
wuooyun <30884739+wuooyun@users.noreply.github.com>
2025-02-17 09:35:20 +0800

    Update main.rs
    add quick url  print

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,40 @@ async fn main() -> std::io::Result<()> {
     let addr = matches.value_of("addr").unwrap();
     let port = matches.value_of("port").unwrap();
     let bind_addr = format!("{}:{}", addr, port);
+    let mut ifaces = Vec::new();
+
+    if addr == "0.0.0.0" {
+        
+        ifaces = if_addrs::get_if_addrs()
+            .unwrap_or_else(|e| {
+                error!("Failed to get local interface addresses: {}", e);
+                Default::default()
+            })
+            .into_iter()
+            .map(|iface| iface.ip())
+            .filter(|ip| (ip.is_ipv4()) || ( ip.is_ipv6()))
+            .collect();
+        ifaces.sort();
+    }
+
+    let urls = ifaces
+                .into_iter()
+                .map(|addr|  {
+                    format!("{}:{}", addr, port)}
+                )
+                .map(|addr|  {
+                    format!("http://{addr}")
+                })
+                .map(|url| format!("{}", url))
+                .collect::<Vec<_>>();
+    println!(
+        "Available at (non-exhaustive list):\n    {}\n",
+        urls
+            .iter()
+            .map(|url| url.to_string())
+            .collect::<Vec<_>>()
+            .join("\n    "),
+    );
 
     std::env::set_var(
         "RUST_LOG",
