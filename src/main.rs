535c8b8ffedc6f5f88d21c1dc3973ea5b886b630
Дамјан Георгиевски <gdamjan@gmail.com>
2025-06-16 18:44:55 +0200

    update clap to v4 (a breaking change)

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,7 @@ async fn main() -> std::io::Result<()> {
                 .long("chdir")
                 .value_name("DIRECTORY")
                 .help("directory to serve")
-                .default_value(".")
-                .takes_value(true),
+                .default_value("."),
         )
         .arg(
             clap::Arg::new("addr")
@@ -23,8 +22,7 @@ async fn main() -> std::io::Result<()> {
                 .long("bind")
                 .value_name("ADDRESS")
                 .help("bind address")
-                .default_value("0.0.0.0")
-                .takes_value(true),
+                .default_value("0.0.0.0"),
         )
         .arg(
             clap::Arg::new("port")
@@ -35,9 +33,9 @@ async fn main() -> std::io::Result<()> {
         );
     let matches = app.get_matches();
 
-    let chdir = matches.value_of("chdir").unwrap(); // these shouldn't panic ever, since all have default_value
-    let addr = matches.value_of("addr").unwrap();
-    let port = matches.value_of("port").unwrap();
+    let chdir = matches.get_one::<String>("chdir").unwrap(); // these shouldn't panic ever, since all have default_value
+    let addr = matches.get_one::<String>("addr").unwrap();
+    let port = matches.get_one::<String>("port").unwrap();
     let bind_addr = format!("{}:{}", addr, port);
 
     env_logger::init();
